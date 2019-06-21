cd47f2a632594465100a52ec7ad117b5683ae3b8
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-06-20 23:17:55 -0500

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int rojo;
+for (rojo = '0' ; rojo <= '9'; rojo++)
+putchar(rojo);
+putchar('\n');
+return (0);
+}
