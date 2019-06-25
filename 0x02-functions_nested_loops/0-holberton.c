d47ea9494297dafcc2471894ad3eccd3be293636
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-06-24 22:48:01 -0500

    Prints Holberton with _Putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*main prints out Holberton, follow by New line
+*
+*Return 0
+ */
+
+int main(void)
+{
+  int v;
+  char hol[9] = "Holberton";
+  for(v = 0; v < 9; v++)
+    {
+      _putchar(hol[v]);
+    }
+  _putchar('\n');
+  return (0);
+}
