68a9032b04327dc25a72937f2036a5467244f210
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-06-26 22:52:07 -0500

    The tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,54 +1,37 @@
-#include <stdio.h>
 #include "holberton.h"
-
 /**
- * times_table - prints the 9 times table starting with 0
+ * times_table - prints the 9 times table.
  *
- * Return: nothing
+ * Return: void
+ *
  */
-
 void times_table(void)
 {
-	int fila, col;
+	int q, w, c;
 
-	int un, dec;
-
-	int multi;
-
-		for (fila = 0; fila <= 9; fila++)
+	for (q = 0; q <= 9; q++)
+	{
+		for (w = 0; w <= 9; w++)
 		{
-			for (col = 0; col <= 9; col++)
-			{
-				multi = fila * col;
-			if (multi >= 10)
-			{
-				un = multi  % 10;
-				dec = multi / 10;
-
-				_putchar(' ');
-				_putchar(dec + '0');
-				_putchar(un + '0');
-			}
-			else
-			{
-				if (col != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(multi +'0');
-
-			}
-			if (col != 9)
+			c = (q * w);
+			if (w != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 			}
-			if (col == 9)
+			if (c >= 10)
 			{
-				_putchar('\n');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
+			else if (c < 10 && w != 0)
+			{
+				_putchar(' ');
+				_putchar((c % 10) + '0');
+			}
+			else
+				_putchar((c % 10) + '0');
 		}
-	col = 0;
-		}
+		_putchar('\n');
+	}
 }
-
