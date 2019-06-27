24ebe36a8c43498d072b75fe9d81ca1573d77e37
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-06-26 22:38:37 -0500

    alpha x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char  v, p;
+	int v;
 
-	v = 'a';
+	char p;
 
-	for (p = 0; p < 10; p++)
+	for (v = 0; v < 10; v++)
 	{
-		while (v <= 'z')
+		for (p = 'a'; p <= 'z'; p++)
 		{
-			_putchar(p);
-			p++;
+	_putchar (p);
 		}
 		_putchar('\n');
-		v = 'a';
 	}
 }
