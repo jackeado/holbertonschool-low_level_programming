efa526296be68c68eb2bbed53b8a7f1401aec10c
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-06-28 14:32:23 -0500

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,14 +12,13 @@ int _isupper(int c)
 
 {
 
-	if (c <= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 
 		return (1);
 
-		else if (c <= 'a' && c <= 'z')
+		else if (c >= 'a' && c <= 'z')
 		return (0);
 
 	return (0);
 
 }
-
