9b06ef61774aa4706085763f6739bb002050cb4a
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-02 16:16:17 -0500

    Library

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,9 +4,11 @@
 /**
  * Structs, enums and unions definitions
  * Typedefs
- * Function prototypes       
+ * Function prototypes
  */
 
+
+void _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
