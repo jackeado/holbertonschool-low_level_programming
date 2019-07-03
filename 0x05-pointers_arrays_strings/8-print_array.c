2e058abec3465146af2b6997ea498a41b4cf5d36
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-03 00:04:13 -0500

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,15 +1,29 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * print_array - an array of integers, followed by a new line.
+ *
+ * @n: value for array
+ * @a: Pointer for the  string
+ *
+ */
+
+
+
 
 void print_array(int *a, int n)
 
 {
 	int alt;
-	for(alt = 0; alt < n; alt++)
-		{
-			if(alt < n)
-				printf("%d, ", a[alt]);
-		}	
+
+	for (alt = 0; alt < n; alt++)
+	{
+		if (alt < n)
+
+	printf("%d, ", a[alt]);
+
 	putchar('\n');
+
+	}
 }
