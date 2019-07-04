3d6a444f7244aa87aa8790895a2819f7faab1d32
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-04 18:10:17 -0500

    String C

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - function that compares two strings
+ *
+ * @s1: string number 1
+ * @s2: string number 2
+ *
+ * Return: j
+ */
+
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int i, j;
+
+	j = 0;
+
+	for (i = 0; s1[i] != '\0' ||  s2[i] != '\0'; i++)
+	{
+		j = s1[i] - s2[i];
+		break;
+	}
+	return (j);
+}
