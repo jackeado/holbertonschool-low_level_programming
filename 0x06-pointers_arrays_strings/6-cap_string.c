2e421879369bb732b8d55957c30a009ad39f1c21
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-04 23:46:32 -0500

    betty fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,7 @@ char *cap_string(char *n)
 {
 	int i;
 	int p = 0;
+
 	for (i = 0; n[i] != 0; i++)
 	{
 		if (n[i] == ' ' || n[i] == '\t' || n[i] == '\n'
@@ -20,7 +21,8 @@ char *cap_string(char *n)
 		{
 			p = 1;
 		}
-		else if ((n[i] >= 97 && n[i] <= 122 && p == 1) || (n[0] >= 97 && n[0] <= 122))
+		else if ((n[i] >= 97 && n[i] <= 122 && p == 1) ||
+			 (n[0] >= 97 && n[0] <= 122))
 		{
 			n[i] = n[i] - 32;
 			p = 0;
