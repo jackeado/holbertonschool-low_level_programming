2b9e384f2afec8316f717416a45491f6d2e74970
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-04 22:47:53 -0500

    Strings of caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,33 @@
-#include <stdio.h>
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * *cap_string - capitalizes all words of a string.
  * @n: The function's parameter
  *
  * Return: n.
  */
-
 char *cap_string(char *n)
-
 {
-	int i, j;
-
-	i = 0;
-
-	for (i = 0; n[i] != '\0'; i++)
+	int i;
+	int p = 0;
+	for (i = 0; n[i] != 0; i++)
 	{
-		if (n[i] == ' ' || n[i] == '\t' || n[i] == '\n' || n[i] == ',' ||
-		    n[i] == ';' || n[i] == '!' || n[i] == '?' || n[i] == '"' ||
-		    n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
+		if (n[i] == ' ' || n[i] == '\t' || n[i] == '\n'
+|| n[i] == ',' || n[i] == ';' || n[i] == '.'
+|| n[i] == '!' || n[i] == '?' || n[i] == '"'
+|| n[i] == '(' || n[i] == ')' || n[i] == '{'
+		    || n[i] == '}')
 		{
-			j = 1;
+			p = 1;
 		}
-		else if ((n[i] >= 97  && n[i] <= 122 && j == 1) ||
-			(n[0] >= 97 && n[0] <= 122))
-
+		else if ((n[i] >= 97 && n[i] <= 122 && p == 1) || (n[0] >= 97 && n[0] <= 122))
 		{
-			n[i] = n[i] -= 32;
-			j = 0;
+			n[i] = n[i] - 32;
+			p = 0;
 		}
 		else
 		{
-			j = 0;
+			p = 0;
 		}
 	}
 	return (n);
