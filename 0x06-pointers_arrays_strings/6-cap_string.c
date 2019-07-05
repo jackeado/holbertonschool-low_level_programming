0883218efc89c4fae3c9e779e008275cd4ec6f3b
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-04 22:40:44 -0500

    Betty Fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+ * *cap_string - capitalizes all words of a string.
+ * @n: The function's parameter
+ *
+ * Return: n.
+ */
+
 char *cap_string(char *n)
 
 {
@@ -11,11 +18,13 @@ char *cap_string(char *n)
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		if (n[i] == ' ' || n[i] == '\t' || n[i] == '\n' || n[i] == ',' ||
-		    n[i] == ';' || n[i] == '!' || n[i] == '?' || n[i] == '"' || n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
+		    n[i] == ';' || n[i] == '!' || n[i] == '?' || n[i] == '"' ||
+		    n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
 		{
 			j = 1;
 		}
-		else if((n[i] >= 97  && n[i] <= 122 && j == 1)||(n[0] >= 97 && n[0] <= 122))
+		else if ((n[i] >= 97  && n[i] <= 122 && j == 1) ||
+			(n[0] >= 97 && n[0] <= 122))
 
 		{
 			n[i] = n[i] -= 32;
