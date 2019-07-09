03e6642ec2aff7c47f7ce0918e099f3b1fa32e37
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-08 22:30:32 -0500

    fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to check
+ * @accept: string to check against
  *
- *
- *
- *
+ * Return: pointer
  */
 
 char *_strpbrk(char *s, char *accept)
@@ -12,17 +14,16 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int q, w;
 
 	q = 0;
-	while(s[q])
+	while (s[q])
 	{
 		w = 0;
-			while(accept[w])
-			{
-				if(s[q] == accept[w])
-					return(s + q);
+		while (accept[w])
+		{
+				if (s[q] == accept[w])
+					return (s + q);
 				w++;
-			}
+		}
 		q++;
 	}
-	return(0);
-
+	return (0);
 }
