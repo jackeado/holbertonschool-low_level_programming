49df3cdfb9840c1a8e48c9a5633b6a74254e1e97
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-10 12:33:21 -0500

    prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s == 0)
+	{	return;
+		
+	}
+	else
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+	return;
+}
