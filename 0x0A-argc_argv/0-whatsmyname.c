764d2720f3e7a04d6a1cab88f6be859fbc9face7
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-14 13:48:06 -0500

    prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- *
- *
+ * Return: 0
  */
 
-int  main (int argc, char *argv[])
+int  main(int argc, char *argv[])
 
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 	printf("%s\n", argv[i]);
 	}
-	return(0);
+	return (0);
 
 }
