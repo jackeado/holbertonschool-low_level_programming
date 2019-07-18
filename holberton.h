0fe1926c43997559e88b402e3a74e9cf8dfd9682
jackeado <45445863+jackeado@users.noreply.github.com>
2019-07-18 12:42:35 -0500

    Delete holberton.h

--- holberton.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-
-
-#endif
