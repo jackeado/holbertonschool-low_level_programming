0a60231a83ae4dd7ec9fef6bf7c955d6df5ab859
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-24 23:28:53 -0500

    main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		printf("Error,\n", exit(1));
+	if atoi(argv(1) < 0)
+		       printf("Error,\n", exit(2));
+	return (0);
+}
