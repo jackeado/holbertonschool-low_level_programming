fb27b768f56db0013f7b34d868e6d3320dc03d72
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-07-24 23:29:58 -0500

    main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,3 @@
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- *
- * Return: always O
- */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
