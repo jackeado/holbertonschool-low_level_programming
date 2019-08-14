90b3770f6e7bf70ee79f2962cc91b2fb3254d5e1
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-08-13 19:37:42 -0500

    function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * create_file - function that creates a file
+ * @filename: file name
+ * @text_content: content file
+ *
+ * Return: int
+ */
+
+int create_file(const char *filename, char *text_content)
+
+{
+	int wtf = 0;
+	int fd = 0;
+	ssize_t i = 0;
+
+	if(filename == NULL)
+	{
+		return(-1);
+	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return(-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
+	wtf = write(fd, text_content, i);
+	if ((wtf == -1) || wtf != i)
+	{
+		return (-1);
+	}
+	return(1);
+}
