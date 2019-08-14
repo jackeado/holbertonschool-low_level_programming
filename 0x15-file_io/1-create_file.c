7d79cf8636887ae22c8dc6357b2a2dd025724730
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-08-13 19:40:02 -0500

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,14 +19,14 @@ int create_file(const char *filename, char *text_content)
 	int fd = 0;
 	ssize_t i = 0;
 
-	if(filename == NULL)
+	if (filename == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
-		return(-1);
+		return (-1);
 	}
 	if (text_content != NULL)
 	{
@@ -38,5 +38,5 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	return(1);
+	return (1);
 }
