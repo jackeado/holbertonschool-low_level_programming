39c2429b1ba0d7e6b86c6059e165d17cbe80ab9a
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-09-14 16:48:25 -0500

    returns the number of elements in a linked

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+
+{
+        size_t i;
+
+        for (i = 0; h != NULL; i++)
+        {
+                h = h->next;
+        }
+        return (i);
+}
+
+
