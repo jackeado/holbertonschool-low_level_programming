9e9b04a4c81e40bf1c02269276a5052df2d17a1d
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-12-04 21:12:29 -0500

    function that inserts a node as the left-child of another node

---
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * @parent: is a pointer to the node to insert the left-child in
+ * @value: is the value to store in the new node
+ * Return: the node left
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node_left;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node_left = binary_tree_node(parent, value);
+	if (node_left == NULL)
+		return (NULL);
+	node_left->left = parent->left;
+	if (node_left->left != NULL)
+		node_left->left->parent = node_left;
+	parent->left = node_left;
+	return (node_left);
+}
