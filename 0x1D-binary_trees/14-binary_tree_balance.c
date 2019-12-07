66ea20a6c56f36d15444ec73bd8650497d289320
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-12-06 20:26:05 -0500

    func that measures the balance factor of a binary tree

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -9,21 +9,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_n = 0;
-    size_t right_n = 0;
+	size_t left_n = 0;
+	size_t right_n = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left != NULL)
-        left_n = binary_tree_height(tree->left) + 1;
-    if (tree->right != NULL)
-        right_n = binary_tree_height(tree->right) + 1;
+	if (tree->left != NULL)
+		left_n = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		right_n = binary_tree_height(tree->right) + 1;
 
-    if (right_n > left_n)
-        return (right_n);
-    else
-        return (left_n);
+	if (right_n > left_n)
+		return (right_n);
+	else
+		return (left_n);
 }
 
 /**
