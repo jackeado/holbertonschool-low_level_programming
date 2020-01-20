a0e31dfc8d393f62f3119a6cb9eb668b7ab9c39b
jackeado <pabsanchez@gmail.com>
2020-01-19 19:56:11 -0500

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,23 +6,23 @@
  *
  * Return: pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_table_t *hashTable;
+	unsigned long int j;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	hashTable = malloc(sizeof(hash_table_t));
+	if (hashTable == NULL)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_table->array == NULL)
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hashTable->array == NULL)
 	{
-		free(hash_table);
+		free(hashTable);
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return (hash_table);
+	for (j = 0; j < size; j++)
+		hashTable->array[j] = NULL;
+	return (hashTable);
 }
